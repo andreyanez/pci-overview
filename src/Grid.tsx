fba5ee5dac354eed79229221f76b13d380f3e866
andreyanez <andre.yanez22@gmail.com>
2023-02-21 10:15:40 -0500

    Task #2

--- src/Grid.tsx
+++ src/Grid.tsx
@@ -1,32 +1,73 @@
-import { AgGridReact } from "ag-grid-react";
-import { ColDef } from "ag-grid-community";
-import data from "./near-earth-asteroids.json";
-import "ag-grid-community/styles/ag-grid.css";
-import "ag-grid-community/styles/ag-theme-alpine.css";
+import { AgGridReact } from 'ag-grid-react';
+import { ColDef } from 'ag-grid-community';
+import data from './near-earth-asteroids.json';
+import 'ag-grid-community/styles/ag-grid.css';
+import 'ag-grid-community/styles/ag-theme-alpine.css';
+import { useMemo } from 'react';
+
+const numberComparator = (valueA: number, valueB: number): number => {
+	return valueA - valueB;
+};
+
+const stringComparator = (valueA: string, valueB: string): number => {
+	if (valueA === valueB) return 0;
+	return valueA > valueB ? 1 : -1;
+};
+
+const dateComparator = (valueA: string, valueB: string): number => {
+	const dateA = new Date(valueA).getTime();
+	const dateB = new Date(valueB).getTime();
+	return dateA - dateB;
+};
 
 const columnDefs: ColDef[] = [
-  { field: "designation", headerName: "Designation" },
-  { field: "discovery_date", headerName: "Discovery Date" },
-  { field: "h_mag", headerName: "H (mag)" },
-  { field: "moid_au", headerName: "MOID (au)" },
-  { field: "q_au_1", headerName: "q (au)" },
-  { field: "q_au_2", headerName: "Q (au)" },
-  { field: "period_yr", headerName: "Period (yr)" },
-  { field: "i_deg", headerName: "Inclination (deg)" },
-  { field: "pha", headerName: "Potentially Hazardous" },
-  { field: "orbit_class", headerName: "Orbit Class", enableRowGroup: true, },
+	{
+		field: 'designation',
+		headerName: 'Designation',
+		comparator: stringComparator,
+	},
+	{
+		field: 'discovery_date',
+		headerName: 'Discovery Date',
+		comparator: dateComparator,
+	},
+	{
+		field: 'h_mag',
+		headerName: 'H (mag)',
+		comparator: numberComparator,
+	},
+	{ field: 'moid_au', headerName: 'MOID (au)', comparator: numberComparator },
+	{ field: 'q_au_1', headerName: 'q (au)', comparator: numberComparator },
+	{ field: 'q_au_2', headerName: 'Q (au)', comparator: numberComparator },
+	{ field: 'period_yr', headerName: 'Period (yr)', comparator: numberComparator },
+	{ field: 'i_deg', headerName: 'Inclination (deg)', comparator: numberComparator },
+	{ field: 'pha', headerName: 'Potentially Hazardous', comparator: stringComparator },
+	{
+		field: 'orbit_class',
+		headerName: 'Orbit Class',
+		enableRowGroup: true,
+		comparator: stringComparator,
+	},
 ];
 
 const NeoGrid = (): JSX.Element => {
-  return (
-    <div className="ag-theme-alpine" style={{ height: 900, width: 1920 }}>
-      <AgGridReact
-        rowData={data}
-        columnDefs={columnDefs}
-        rowGroupPanelShow={'always'}
-      />
-    </div>
-  );
+	const defaultColDef = useMemo(() => {
+		return {
+			filter: true,
+			sortable: true,
+		};
+	}, []);
+
+	return (
+		<div className="ag-theme-alpine" style={{ height: 900, width: 1920 }}>
+			<AgGridReact
+				rowData={data}
+				columnDefs={columnDefs}
+				defaultColDef={defaultColDef}
+				rowGroupPanelShow={'always'}
+			/>
+		</div>
+	);
 };
 
 export default NeoGrid;
